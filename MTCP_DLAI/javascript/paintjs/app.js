ccdb36641debf38e6eded319bd84e320720baecc
JSIncognito <junsoo3218@gmail.com>
2022-05-14 02:48:22 +0900

    2D Context

--- MTCP_DLAI/javascript/paintjs/app.js
+++ MTCP_DLAI/javascript/paintjs/app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,26 +10,39 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMousMove(event) {
   //   console.log(event);
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMousDown(event) {
   painting = true;
 }
-function onMousUp(event) {
-  //   painting = false;
-  stopPainting();
-}
+
+// function onMousUp(event) {
+//   //   painting = false;
+//   stopPainting();
+// }
+
 function onMousLeave(event) {
   painting = false;
 }
 if (canvas) {
   canvas.addEventListener('mousemove', onMousMove);
-  canvas.addEventListener('mousedown', onMousDown);
-  canvas.addEventListener('mouseup', onMousUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
   //   canvas.addEventListener('mouseleave', onMousLeave);
 }
